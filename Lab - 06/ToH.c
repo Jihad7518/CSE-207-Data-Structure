d6cb2a5b63675503fbb9168f45cf192b327644ff
jihad7518 <jihadmd7518@gmail.com>
2023-07-01 17:12:45 +0600

    .

---
+++ Lab - 06/ToH.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int TOWER(int N, char A, char B, char C){
+
+    if(N==1)
+    {
+        printf("\n %d disc is moving from %c to %c \n",N,A,C);
+    }
+
+    else {
+
+        TOWER(N-1, A,C,B);
+        printf("\n %d disc is moving from %c to %c \n",N,A,C);
+        TOWER(N-1, B,A,C);
+
+    }
+}
+
+
+int main(){
+
+    int N;
+    printf("How many disc:");
+    scanf("%d",&N);
+
+    TOWER(N,'a','b','c');
+
+
+return 0;
+}
+
